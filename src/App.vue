f5d0a5ac3933843ec3f93e0c804b993b43f691ab
Alex Tindall <tindall.al@gmail.com>
2024-11-13 10:08:16 -0600

    added comment

--- src/App.vue
+++ src/App.vue
@@ -67,6 +67,15 @@ export default {
       </div> -->
       <div class="container-fluid mt-3">
         <library-item-list :library="results"></library-item-list>
+
+        <!-- <div v-for="item in results" :key="item">
+          <div class="row mt-4 card">
+            <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6" style="height: 200px;">
+              {{ item }}
+            </div>
+          </div>
+        </div> -->
+
       </div>
     </div>
   </div>
